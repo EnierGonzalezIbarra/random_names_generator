6ffea3d41a7d230a18543bb649fc73c3c69625e9
Enier González Ibarra <gonzalezibarraenier@gmail.com>
2025-07-12 20:09:07 -0400

    add cli for number

--- gen_names.py
+++ gen_names.py
@@ -5,15 +5,19 @@ import names_and_lastnames
 import argparse
 
 parser = argparse.ArgumentParser(description="Generate a list of random names.")
+
+# Add required positional argument for the number of names
+parser.add_argument("num_names", type=int, help="The number of names to generate.")
+
 parser.add_argument(
     "name_structure",
-    nargs="?", # Makes the argument optional
-    default="fl",
-    help="Name structure to generate (e.g., 'fl' for first and last name, 'f' for first name, 'f2l' for first and two last names). Defaults to 'fl'.",
+    help="Name structure to generate (e.g., 'fl' for first and last name, 'f' for first name, 'f2l' for first and two last names).",
 )
 
 args = parser.parse_args()
 
-names = [names_and_lastnames.get_random(args.name_structure) for _ in range(100)]
+names = [
+    names_and_lastnames.get_random(args.name_structure) for _ in range(args.num_names)
+]
 for name in names:
     print(name)
